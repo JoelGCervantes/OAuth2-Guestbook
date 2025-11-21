72c5b75ab990e74ca6d4101ec38c06143bd60963
Joel <garciacervantesj00@gmail.com>
2025-11-21 18:47:08 +0000

    doc string

--- oauth_config.py
+++ oauth_config.py
@@ -1,3 +1,5 @@
+#configuration file that reads in the environment variables set in the shell and contains the predefined URLs that Google uses for its identity provider endpoints.
+
 import os
 client_id = os.environ.get('CLIENT_ID')
 client_secret = os.environ.get('CLIENT_SECRET')
